c9817cda47a8d541d9052da0da8104bf746f63ac
loyd <loyd@users.noreply.github.com>
2026-03-27 11:05:14 +0000

    deploy: b79bd547e1eef4d9c8c510dc2e6f631035b180e4

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3a78dee6.js',
+                path_to_root + 'searchindex-383c83a6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
